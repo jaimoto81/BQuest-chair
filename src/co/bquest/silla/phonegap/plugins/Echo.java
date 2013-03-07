b108e76ba1656d31008218cb73193dfc9f4304bd
jaimoto81@gmail.com <jaimoto81@gmail.com>
2013-03-07 00:54:27 +0000

    Twitter Plugin

--- src/co/bquest/silla/phonegap/plugins/Echo.java
+++ src/co/bquest/silla/phonegap/plugins/Echo.java
@@ -5,6 +5,11 @@ import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -14,14 +19,27 @@ public class Echo extends CordovaPlugin {
         if (action.equals("echo")) {
             String message = args.getString(0); 
             this.echo(message,callbackContext);
+            Twitter twitter = TwitterFactory.getSingleton();
+       	 	try {
+       	 		twitter.toString();
+				//twitter.updateStatus(message);
+			} catch (Exception e) {
+				callbackContext.error("Expected one non-empty string argument.");
+				return false;
+			}
+       	 
+            
             return true;
         }
         return false;
     }
+    
+    
 
     private void echo(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) { 
-            callbackContext.success(message + " World!!");
+        	//System.out.println("Successfully updated the status to [" + status.getText() + "].");
+            callbackContext.success(message + " Worldis!!");
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
